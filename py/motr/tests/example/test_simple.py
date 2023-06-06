c7e2d88a6d248d9acacd52b8a34387e97ea62e6a
fengttt <fengttt@gmail.com>
2023-06-05 21:00:28 -0700

    rename.

---
+++ py/motr/tests/example/test_simple.py
@@ -0,0 +1,44 @@
+import altair as alt
+import numpy as np
+import pandas as pd
+import motr
+import pytest
+from vega_datasets import local_data, data
+
+@pytest.fixture(scope="module")
+def motr_init():
+    motr.enable('mysql+pymysql://dump:111@localhost:6001/mojo')
+
+def test_simple_bar_char(motr_init):
+    source = pd.DataFrame({
+            'a': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+                'b': [28, 55, 43, 91, 81, 53, 19, 87, 52]
+                })
+
+    motr.from_pd(source, 'simple')
+    chart = alt.Chart('motr://simple').mark_bar().encode(
+               x='a:N',
+               y='b:Q'
+    )
+
+    chart = motr.transform_chart(chart)
+    chart.save("simple_bar_chart.png")
+
+def test_simple_heatmap(motr_init):
+    # Compute x^2 + y^2 across a 2D grid
+    x, y = np.meshgrid(range(-5, 5), range(-5, 5))
+    z = x ** 2 + y ** 2
+
+    # Convert this grid to columnar data expected by Altair
+    source = pd.DataFrame({'x': x.ravel(),
+                     'y': y.ravel(),
+                     'z': z.ravel()})
+
+    motr.from_pd(source, 'simple')
+    chart = alt.Chart("motr://simple").mark_rect().encode(
+        x='x:O',
+        y='y:O',
+        color='z:Q'
+    )
+    chart = motr.transform_chart(chart)
+    chart.save("simple_heatmap_chart.png")
--- py/motr/tests/util.py
+++ py/motr/tests/util.py
@@ -1,3 +1,4 @@
+import sys
 import sqlalchemy
 from sqlalchemy import create_engine
 from vega_datasets import local_data, data
@@ -20,6 +21,15 @@ def load_vega_datasets():
         df = data(d)
         df.to_sql(tbl, e, if_exists='replace', index=False)
 
+def load_data(name):
+    e = create_engine('mysql+pymysql://dump:111@localhost:6001/mojo')
+    tbl = name.replace('-', '_')
+    print("loading {0} into {1}".format(name, tbl))
+    df = data(name)
+    df.to_sql(tbl, e, if_exists='replace', index=False)
 
 if __name__ == "__main__":
-    load_vega_datasets()
+    if len(sys.argv) == 1:
+        load_vega_datasets()
+    else:
+        load_data(sys.argv[1])
